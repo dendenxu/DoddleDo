32122c1ff90d183cf07ebc78f73f56518b0de82f
dendenxu <56295612@qq.com>
2019-08-02 20:25:08 +0800

    undo feature temporarily finished beware that currently the finish button is used to test the undo feature since I haven't dived into combining the bottons with custom gestures and I possess no undo button image... and I've also learn that their may exists faster way of rendering image context called: UIGraphicsImageRenderer
    url: www.hackingwithswift.com/read/27/3/drawing-into-a-core-graphics-context-with-uigraphicsimagerenderer

--- DoddleDo/DoddleDo/DoddleBoardViewController.swift
+++ DoddleDo/DoddleDo/DoddleBoardViewController.swift
@@ -40,12 +40,38 @@ class DoddleBoardViewController: UIViewController {
     }
 
     @IBOutlet weak var mainImageView: UIImageView!
+
     @IBOutlet weak var tempImageView: UIImageView!
+
+    // TEMPORARILY USING FINISH AS UNDO
+    @IBOutlet weak var finish: ShadowedImageView! {
+        didSet {
+            let tapOrPress = UILongPressGestureRecognizer(target: self, action: #selector(buttonTappedOrPressed(recognizer:)))
+            tapOrPress.minimumPressDuration = 0
+            finish.addGestureRecognizer(tapOrPress)
+            finish.isUserInteractionEnabled = true
+        }
+    }
+
+    var tempImages = [AttributedImage]() {
+        didSet {
+            UIGraphicsBeginImageContext(view.frame.size)
+            mainImageView.image = nil
+            for i in 0..<tempImages.count {
+                let tempImage = tempImages[i]
+                tempImage.image?.draw(in: view.bounds, blendMode: tempImage.blendMode, alpha: tempImage.opacity)
+            }
+            mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+            mainImageView.alpha = 1.0
+            UIGraphicsEndImageContext()
+        }
+    }
 
     var lastPoint = CGPoint.zero
     var color = UIColor.blue
     var brushWidth: CGFloat = 30.0
     var opacity: CGFloat = 1.0
+    var blendMode = CGBlendMode.normal
     private var points = [CGPoint]()
 
     func drawLine(from fromPoint: CGPoint, to toPoint: CGPoint) {
@@ -94,23 +120,29 @@ class DoddleBoardViewController: UIViewController {
         drawLine(from: lastPoint, to: lastPoint)
         tempImageView.image = nil
 
+        let tempImage = AttributedImage()
         UIGraphicsBeginImageContext(view.frame.size)
-        mainImageView.image?.draw(in: view.bounds)
+//        mainImageView.image?.draw(in: view.bounds)
         guard let context = UIGraphicsGetCurrentContext() else { return }
         context.addPath(computePath(of: points, with: 6))
         if points.count > 2 {
             context.move(to: points[points.count - 2])
             context.addLine(to: points[points.count - 1])
+            context.move(to: points[0])
+            context.addLine(to: points[1])
         }
         context.setStrokeColor(color.cgColor)
         context.setLineWidth(brushWidth)
         context.setLineCap(.round)
         context.setBlendMode(.normal)
         context.strokePath()
-        mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
-        mainImageView.alpha = opacity
+//        mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+//        mainImageView.alpha = opacity
+        tempImage.image = UIGraphicsGetImageFromCurrentImageContext()
+        tempImage.opacity = opacity
+        tempImage.blendMode = .normal
         UIGraphicsEndImageContext()
-
+        tempImages.append(tempImage)
         points = [CGPoint]()
     }
 
@@ -131,4 +163,11 @@ extension CGPoint {
     static func mid(of point1: CGPoint, and point2: CGPoint) -> CGPoint {
         return CGPoint(x: (point1.x + point2.x) / 2, y: (point1.y + point2.y) / 2)
     }
+}
+
+
+class AttributedImage {
+    var opacity: CGFloat = 1.0
+    var blendMode = CGBlendMode.normal
+    var image: UIImage?
 }
