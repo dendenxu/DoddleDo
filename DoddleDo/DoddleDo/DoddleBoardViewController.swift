d3264950cddde57da0b2101e2d509cb44e4606bf
dendenxu <56295612@qq.com>
2019-08-02 19:15:06 +0800

    add doddle board essential stuff it's quite easy to simple draw something on the screen like a board, but it's not easy to smoothen those drawn lines. I've adopted a simpler way to compute the beizier path specified by your fast-moving finger. guess the smoothen part is one part that this app is better than the AI Painting website

--- DoddleDo/DoddleDo/DoddleBoardViewController.swift
+++ DoddleDo/DoddleDo/DoddleBoardViewController.swift
@@ -30,12 +30,105 @@ class DoddleBoardViewController: UIViewController {
         view.addGestureRecognizer(doubleTap)
         view.addGestureRecognizer(longPress)
     }
-    
+
     @objc func doubleTapped(recognizer: UITapGestureRecognizer) {
-        
+
     }
-    
+
     @objc func longPressed(recognizer: UILongPressGestureRecognizer) {
-        
+
+    }
+
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var tempImageView: UIImageView!
+
+    var lastPoint = CGPoint.zero
+    var color = UIColor.blue
+    var brushWidth: CGFloat = 30.0
+    var opacity: CGFloat = 1.0
+    private var points = [CGPoint]()
+
+    func drawLine(from fromPoint: CGPoint, to toPoint: CGPoint) {
+        UIGraphicsBeginImageContext(view.frame.size)
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        tempImageView.image?.draw(in: view.bounds)
+        context.move(to: fromPoint)
+        context.addLine(to: toPoint)
+        context.setLineCap(.round)
+        context.setBlendMode(.normal)
+        context.setStrokeColor(color.cgColor)
+        context.setLineWidth(brushWidth)
+        context.strokePath()
+        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        tempImageView.alpha = opacity
+        UIGraphicsEndImageContext()
+    }
+
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+
+        lastPoint = touch.location(in: view)
+        points.append(lastPoint)
+
+        drawLine(from: lastPoint, to: lastPoint)
+
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+
+        let currentPoint = touch.location(in: view)
+        points.append(currentPoint)
+
+        drawLine(from: lastPoint, to: currentPoint)
+
+        lastPoint = currentPoint
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let currentPoint = touch.location(in: view)
+        points.append(currentPoint)
+
+        drawLine(from: lastPoint, to: lastPoint)
+        tempImageView.image = nil
+
+        UIGraphicsBeginImageContext(view.frame.size)
+        mainImageView.image?.draw(in: view.bounds)
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        context.addPath(computePath(of: points, with: 6))
+        if points.count > 2 {
+            context.move(to: points[points.count - 2])
+            context.addLine(to: points[points.count - 1])
+        }
+        context.setStrokeColor(color.cgColor)
+        context.setLineWidth(brushWidth)
+        context.setLineCap(.round)
+        context.setBlendMode(.normal)
+        context.strokePath()
+        mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        mainImageView.alpha = opacity
+        UIGraphicsEndImageContext()
+
+        points = [CGPoint]()
+    }
+
+    private func computePath(of points: [CGPoint], with factor: CGFloat) -> CGPath {
+        let path = UIBezierPath()
+        let tempPoints = [points[1]] + points + [points[points.count - 1]]
+        path.move(to: points[0])
+        for i in 1..<tempPoints.count - 2 {
+            let control1 = CGPoint(x: tempPoints[i].x + (tempPoints[i + 1].x - tempPoints[i - 1].x) / factor, y: tempPoints[i].y + (tempPoints[i + 1].y - tempPoints[i - 1].y) / factor)
+            let control2 = CGPoint(x: tempPoints[i + 1].x - (tempPoints[i + 2].x - tempPoints[i].x) / factor, y: tempPoints[i + 1].y - (tempPoints[i + 2].y - tempPoints[i].y) / factor)
+            path.addCurve(to: tempPoints[i + 1], controlPoint1: control1, controlPoint2: control2)
+        }
+        return path.cgPath
+    }
+}
+
+extension CGPoint {
+    static func mid(of point1: CGPoint, and point2: CGPoint) -> CGPoint {
+        return CGPoint(x: (point1.x + point2.x) / 2, y: (point1.y + point2.y) / 2)
     }
 }
